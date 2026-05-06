7d8a74a26e04963640dd375dd97521bbb906cef1
Chris West <thrillgraphics@gmail.com>
2026-05-05 20:30:47 -0400

    Add outcome legend to /compare top-right

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -17,6 +17,7 @@ import { CompareLinkActions } from "./CompareLinkActions";
 import { CompareSlotSearch } from "./CompareSlotSearch";
 import { TunnelingPanel } from "./TunnelingPanel";
 import { CompareHoverProvider, HoverableSide } from "./CompareHoverContext";
+import { OutcomeLegend } from "./OutcomeLegend";
 
 interface PageProps {
   searchParams: Promise<{
@@ -147,6 +148,8 @@ export default async function ComparePage({ searchParams }: PageProps) {
             Compare
           </div>
         </header>
+
+        <OutcomeLegend />
 
         <section className="absolute top-20 left-6 w-[400px] rounded-lg bg-white/[0.06] backdrop-blur-md border border-white/10 shadow-lg p-4 space-y-4 pointer-events-auto max-h-[calc(100vh-7rem)] overflow-y-auto">
           <HoverableSide side="a">
